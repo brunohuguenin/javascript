c2ea6a0a900063e2481a7b397264e01d72131b3c
Brunohugueninn <brunohugueninn@gmail.com>
2023-10-30 21:24:22 -0300

    primeiro commit

---
+++ modulo-02/variaveis/exercicio.js
@@ -0,0 +1,20 @@
+// Declarar uma variável com o seu nome
+var nome = 'Bruno';
+
+// Declarar uma variável com a sua idade
+var idade = 25;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+var comidaFavorita = 'Strogonof';
+
+// Declarar 5 variáveis diferentes sem valores
+var profissao;
+let time;
+const ala;
+let estado;
+var pais;
---
+++ modulo-02/variaveis/script.js
@@ -0,0 +1,15 @@
+var nome = 'Bruno';
+var idade = 28;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+// var time = 'Vasco';
+const time = 'Fluminense';
+time = 'Flamengo';
+console.log(time);
