f284c8e32bb9f9b20140b4bdcf4a80b499e31a22
Brunohugueninn <brunohugueninn@gmail.com>
2023-10-30 22:11:32 -0300

    aula 03

---
+++ modulo-02/numeros-operadores/script.js
@@ -0,0 +1,12 @@
+var idade = 25;
+console.log(idade);
+
+var testeNaN = '1400' / 2;
+console.log(isNaN(testeNaN));
+
+var soma = 10 + 10 + 20 + (30 * 4) / (2 + 10);
+console.log(soma);
+
+var x = 5;
+console.log(x--);
+console.log(x);
