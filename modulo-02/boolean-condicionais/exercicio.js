d2f85800648c97dcf29a97fec2ed8bed96933e75
Brunohugueninn <brunohugueninn@gmail.com>
2023-10-30 23:05:43 -0300

    aula 4

---
+++ modulo-02/boolean-condicionais/exercicio.js
@@ -0,0 +1,54 @@
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var minhaIdade = 25;
+var idadeIrmao = 20;
+
+if (minhaIdade > idadeIrmao) {
+  console.log('Eu sou mais velho.');
+} else if (minhaIdade == idadeIrmao) {
+  console.log('Temos a mesma idade.');
+} else {
+  console.log('Meu irmão é mais velho.');
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = 5 - 2 && 5 - ' ' && 5 - 2;
+console.log(!!expressao);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+console.log(
+  !!nome,
+  !!idade,
+  !!possuiDoutorado,
+  !!empregoFuturo,
+  !!dinheiroNaConta,
+);
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if (brasil > china) {
+  console.log('Brasil tem mais habitantes do que a China');
+} else {
+  console.log('A China tem mais habitantes do que o Brasil');
+}
+
+// O que irá aparecer no console?
+if ('Gato' === 'gato' && 5 > 2) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+
+// O que irá aparecer no console?
+if ('Gato' === 'gato' || 5 > 2) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
---
+++ modulo-02/boolean-condicionais/script.js
@@ -0,0 +1,16 @@
+var possuiGraduacao = false;
+var possuiDoutorado = true;
+
+if (!possuiGraduacao) {
+  console.log('Possui Graduação.');
+} else if (possuiDoutorado) {
+  console.log('Possui Doutorado.');
+} else {
+  console.log('Não Possui Nada.');
+}
+
+if (5 - 5 && 5 + 5) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Flaso');
+}
