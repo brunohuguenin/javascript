d2637fdf1193b3e17012884507a649d82aa576ce
Brunohugueninn <brunohugueninn@gmail.com>
2023-11-26 11:24:33 -0300

    aula 45

--- modulo-05/formularios/formulario-02/script.js
+++ modulo-05/formularios/formulario-02/script.js
@@ -1,6 +1,7 @@
 const controles = document.getElementById('controles');
 const cssText = document.querySelector('.css');
 const btn = document.querySelector('.btn');
+controles.addEventListener('change', handleChange);
 
 const handleStyle = {
   element: btn,
@@ -38,10 +39,26 @@ function handleChange(event) {
   const value = event.target.value;
 
   handleStyle[name](value);
+  saveValues((name, value));
   showCss();
 }
 
-controles.addEventListener('change', handleChange);
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach((property) => {
+    if (handleStyle[property] && typeof handleStyle[property] === 'function') {
+      handleStyle[property](localStorage[property]);
+      controles.elements[property].value = localStorage[property];
+    }
+  });
+  showCss();
+}
+
+setValues();
 
 function showCss() {
   cssText.innerHTML =
