a1bd4e79dd6a154a67ae3a7f76881708959e6e6c
Brunohugueninn <brunohugueninn@gmail.com>
2023-12-04 17:16:43 -0300

    aula 52

--- modulo-06/js/script.js
+++ modulo-06/js/script.js
@@ -6,8 +6,9 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdowMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumero from './modules/anima-numeros.js';
+
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/animais-fetch.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -17,5 +18,5 @@ initModal();
 initTooltip();
 initDropdowMenu();
 initMenuMobile();
-initAnimaNumero();
 initFuncionamento();
+initFetchAnimais();
